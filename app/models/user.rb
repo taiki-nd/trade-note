a6b926eed738e9a9a4fc49946489eb4b42e2d772
Taiki Noda <taiki.nd.real@gmail.com>
2021-11-15 22:01:12 +0900

    nicknameのバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, uniqueness: true, format: { with: /\A[a-zA-Z\d]+\z/ }, length: { minimum: 3, maximum: 25 }
 end
--- db/migrate/20211115123823_devise_create_users.rb
+++ db/migrate/20211115123823_devise_create_users.rb
@@ -6,6 +6,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :nickname,           null: false, default: ""
+      t.boolean :admin,             null: false, default: false
+
 
       ## Recoverable
       t.string   :reset_password_token

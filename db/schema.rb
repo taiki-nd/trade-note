0fd9fe429678d2ac64cab0129eed77abc31a1c45
Taiki Noda <taiki.nd.real@gmail.com>
2021-11-18 03:00:43 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_17_170534) do
+ActiveRecord::Schema.define(version: 2021_11_17_175450) do
+
+  create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", charset: "utf8", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", charset: "utf8", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "records", charset: "utf8", force: :cascade do |t|
     t.date "date"
@@ -22,7 +50,7 @@ ActiveRecord::Schema.define(version: 2021_11_17_170534) do
     t.integer "indi_main_id"
     t.integer "indi_sub_id"
     t.text "entry_basis"
-    t.integer "method_id"
+    t.integer "order_id"
     t.float "price_renge"
     t.integer "result_section_id"
     t.integer "result_content"
@@ -47,4 +75,6 @@ ActiveRecord::Schema.define(version: 2021_11_17_170534) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end

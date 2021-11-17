4eb03ed4d7093ba7fc2c13cdbccd49265c00126f
Taiki Noda <taiki.nd.real@gmail.com>
2021-11-18 03:01:23 +0900

    newアクション

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -10,10 +10,19 @@ class RecordsController < ApplicationController
     @record = Record.new
   end
 
+  def create
+    @record = Record.new(record_params)
+    if @record.save
+      redirect_to records_path
+    else
+      render :new
+    end
+  end
+
   private
 
   def record_params
-    params.require(:record).permit(:date, :hour_id, :pair_id, :ashi_id, :line_main_id, :line_sub_id, :indi_main_id, :indi_sub_id, :entry_basis, :method_id, :price_renge, :result_section_id, :result_content).merge(user_id: current_user.id)
+    params.require(:record).permit(:date, :hour_id, :pair_id, :ashi_id, :kind_id,:line_main_id, :line_sub_id, :indi_main_id, :indi_sub_id, :entry_basis, :order_id, :price_renge, :result_section_id, :result_content, :image).merge(user_id: current_user.id)
   end
-  
+
 end

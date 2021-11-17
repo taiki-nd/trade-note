b2a48b5a562a5ef04133597fdd83f773ebb6d014
Taiki Noda <taiki.nd.real@gmail.com>
2021-11-18 01:15:00 +0900

    バリデーション

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,5 +1,23 @@
 class Record < ApplicationRecord
 
-  
+  with_options presence: true do
+    validates :date
+    validates :hour_id
+    validates :pair_id
+    validates :ashi_id
+    validates :price_renge, numericality: true
+    validates :entry_basis
+    validates :method_id
+    validates :result_section_id
+    validates :result_content
+  end
+
+  with_options numericality: { other_than: 1, message: 'を選択してください' } do
+    validates :hour_id
+    validates :pair_id
+    validates :ashi_id
+    validates :method_id
+    validates :result_section_id
+  end
 
 end

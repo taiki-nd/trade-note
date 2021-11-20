4990e19d09ad1db9ae52aee628f08fbe8e15e1f2
Taiki Noda <taiki.nd.real@gmail.com>
2021-11-21 02:18:46 +0900

    通貨ペア別勝率の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,23 +15,28 @@ class UsersController < ApplicationController
     @user = User.find_by(nickname: params[:nickname])
     @records = Record.includes(:user).order("created_at DESC").limit(4)
 
-    @users_records = Record.where(user_id: @user.id).order(:date)
-
+    @users_records = Record.where(user_id: @user.id)
+    
     # 全体
-      # 金額推移
+      # 資金推移
+      @users_records_order_date = @users_records.order(:date)
     @sum_price = []
     sum = 0
-    @users_records.each do |record|
+    @users_records_order_date.each do |record|
       sum = sum + record.price_renge
       date = record.date
       data = [date, sum]
       @sum_price << data
     end
+
       # 勝率
     @win_count = @users_records.where('price_renge > ?', 0).count
     @lose_count = @users_records.where('price_renge < ?', 0).count
     @rate = {"lose": @lose_count, "win": @win_count}
-    
+
+      # 通貨ペア別勝率
+    @users_records_pairs = @users_records.group(:pair_id).pluck(:pair_id)
+
     # 今日
     today = Time.now.day
     @one_day = Record.where('extract(day from date) = ?'), today

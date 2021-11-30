77fd7c3a5de686375bd30e31fd5679dbe8c9a8e0
Taiki Noda <taiki.nd.real@gmail.com>
2021-11-30 17:21:10 +0900

    テーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_17_175450) do
+ActiveRecord::Schema.define(version: 2021_11_30_081514) do
 
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -38,6 +38,16 @@ ActiveRecord::Schema.define(version: 2021_11_17_175450) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "posts", charset: "utf8", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "category"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "records", charset: "utf8", force: :cascade do |t|

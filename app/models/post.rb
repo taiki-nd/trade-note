16298ecce6c4067d0842ab30c6dd08c8640987ac
Taiki Noda <taiki.nd.real@gmail.com>
2021-11-30 20:14:27 +0900

    修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,7 @@ class Post < ApplicationRecord
   end
 
   with_options numericality: { other_than: 1, message: 'を選択してください' } do
-    validates :cotegory_id
+    validates :category_id
   end
 
   extend ActiveHash::Associations::ActiveRecordExtensions

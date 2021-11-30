81b4a8ef031fdacb5708f271303e4235b9012c45
Taiki Noda <taiki.nd.real@gmail.com>
2021-11-30 19:18:35 +0900

    アソシエーション、バリデーション

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+
+  with_options presence: true do
+    validates :title
+    validates :content
+    validates :category_id
+  end
+
+  with_options numericality: { other_than: 1, message: 'を選択してください' } do
+    validates :cotegory_id
+  end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, optional: true
 end

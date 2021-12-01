fc5c7ff997d4455d57841624892b64c97f08b499
Taiki Noda <taiki.nd.real@gmail.com>
2021-12-01 13:56:19 +0900

    topページに記事を表示

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -1,6 +1,7 @@
 class StaticPageController < ApplicationController
   def top
     @records = Record.includes(:user).order(date: "DESC").limit(4)
+    @posts = Post.includes(:user).order("created_at DESC").limit(4)
   end
 
   def privacy

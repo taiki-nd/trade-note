2b80946a0422b82f1c50ac946200b5d907614993
Taiki Noda <taiki.nd.real@gmail.com>
2021-12-01 22:33:46 +0900

    relationshipテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_01_101316) do
+ActiveRecord::Schema.define(version: 2021_12_01_133212) do
 
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -96,6 +96,15 @@ ActiveRecord::Schema.define(version: 2021_12_01_101316) do
     t.index ["user_id"], name: "index_records_on_user_id"
   end
 
+  create_table "relationships", charset: "utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "follow_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["follow_id"], name: "index_relationships_on_follow_id"
+    t.index ["user_id"], name: "index_relationships_on_user_id"
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -114,4 +123,6 @@ ActiveRecord::Schema.define(version: 2021_12_01_101316) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comment_posts", "posts"
   add_foreign_key "comment_records", "records"
+  add_foreign_key "relationships", "users"
+  add_foreign_key "relationships", "users", column: "follow_id"
 end

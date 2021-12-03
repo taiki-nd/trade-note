e6dad4d4a8911120b8cdc6eed779b17516169729
Taiki Noda <taiki.nd.real@gmail.com>
2021-12-03 17:47:44 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  resources :posts do
-    resources :comment_posts
-  end
-  post 'posts/attach', to: 'posts#attach'
-  devise_for :users
+
   root to: 'static_page#top'
   get 'privacy_policy', to: 'static_page#privacy'
   get 'disclaimer', to: 'static_page#disclaimer'
+
+  devise_for :users
+
   resources :users, only: :update
   get '/users/:nickname' => 'users#show', as: :users_show
   get '/users/:nickname/edit' => 'users#edit', as: :users_edit
@@ -16,14 +15,22 @@ Rails.application.routes.draw do
     end
   end
   get '/users/:nickname/records' => 'users#records', as: :users_records
+  get '/users/:nickname/followings' => 'relationships#followings', as: 'followings'
+  get '/users/:nickname/followers' => 'relationships#followers', as: 'followers' 
+  resources :relationships, only: [:create, :destroy]
+  
   resources :records do
     resources :comment_records
     collection do
       get 'search'
     end
   end
-  get '/users/:nickname/followings' => 'relationships#followings', as: 'followings'
-  get '/users/:nickname/followers' => 'relationships#followers', as: 'followers' 
-  resources :relationships, only: [:create, :destroy]
+
+  resources :posts do
+    resources :comment_posts
+  end
+  post 'posts/attach', to: 'posts#attach'
+  
+  resources :advertisements, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

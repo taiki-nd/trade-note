d2662901b75d3d2c634e347eba7314d7353037ad
Taiki Noda <taiki.nd.real@gmail.com>
2021-12-04 00:10:28 +0900

    company.rbの修正

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,10 +1,10 @@
 class Company < ActiveHash::Base
   self.data = [
     { id: 1, name: 'company' },
-    { id: 2, name: 'xm' },
-    { id: 3, name: 'exness' },
-    { id: 4, name: 'titan' },
-    { id: 5, name: 'tradeview' },
+    { id: 2, name: 'XM' },
+    { id: 3, name: 'EXNESS' },
+    { id: 4, name: 'TitanFx' },
+    { id: 5, name: 'Tradeview' },
   ]
  
    include ActiveHash::Associations

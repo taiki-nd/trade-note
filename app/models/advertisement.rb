f9bbd16e5562554b6c51fe578949f70e183e4a29
Taiki Noda <taiki.nd.real@gmail.com>
2021-12-03 18:15:55 +0900

    バリデーション、アソシエーション

--- app/models/advertisement.rb
+++ app/models/advertisement.rb
@@ -1,2 +1,14 @@
 class Advertisement < ApplicationRecord
+
+  with_options presence: true do
+    validates :img_url
+    validates :af_link
+    validates :rate_id
+    validates :company_id
+  end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  has_many :rate
+  has_many :company
+
 end

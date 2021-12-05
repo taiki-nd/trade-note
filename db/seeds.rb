9130a567d059891d910fefece5f61abde1fb73f2
Taiki Noda <taiki.nd.real@gmail.com>
2021-12-06 00:16:17 +0900

    データの投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,228 +8,571 @@
 
 5.times do
   Record.create!(
-    date: "2020-11-01",
+    date: "2021-05-01",
     hour_id: rand(2..5),
-    pair_id: rand(2..5),
+    pair_id: 51,
     ashi_id: rand(2..5),
     order_id: rand(2..5),
     line_main_id: rand(2..5),
     line_sub_id: rand(2..5),
     indi_main_id: rand(2..5),
     indi_sub_id: rand(2..5),
-    price_renge: 25,
+    price_renge: rand(-100..100),
     entry_basis: "text text",
     result_content: "text text",
-    result_section_id: rand(2..5),
+    result_section_id: rand(2..19),
     user_id: 1
   )
 end
 
 5.times do
   Record.create!(
-    date: "2020-11-01",
+    date: "2021-05-02",
     hour_id: rand(2..5),
-    pair_id: rand(2..5),
+    pair_id: 65,
     ashi_id: rand(2..5),
     order_id: rand(2..5),
     line_main_id: rand(2..5),
     line_sub_id: rand(2..5),
     indi_main_id: rand(2..5),
     indi_sub_id: rand(2..5),
-    price_renge: -15,
+    price_renge: rand(-100..100),
     entry_basis: "text text",
     result_content: "text text",
-    result_section_id: rand(2..5),
+    result_section_id: rand(2..19),
     user_id: 1
   )
 end
 
 5.times do
   Record.create!(
-    date: "2020-11-05",
+    date: "2021-05-03",
     hour_id: rand(2..5),
-    pair_id: rand(2..5),
+    pair_id: 91,
     ashi_id: rand(2..5),
     order_id: rand(2..5),
     line_main_id: rand(2..5),
     line_sub_id: rand(2..5),
     indi_main_id: rand(2..5),
     indi_sub_id: rand(2..5),
-    price_renge: 20,
+    price_renge: rand(-100..100),
     entry_basis: "text text",
     result_content: "text text",
-    result_section_id: rand(2..5),
+    result_section_id: rand(2..19),
     user_id: 1
   )
 end
 
 5.times do
   Record.create!(
-    date: "2020-11-05",
+    date: "2021-05-06",
     hour_id: rand(2..5),
-    pair_id: rand(2..5),
+    pair_id: 108,
     ashi_id: rand(2..5),
     order_id: rand(2..5),
     line_main_id: rand(2..5),
     line_sub_id: rand(2..5),
     indi_main_id: rand(2..5),
     indi_sub_id: rand(2..5),
-    price_renge: -22,
+    price_renge: rand(-100..100),
     entry_basis: "text text",
     result_content: "text text",
-    result_section_id: rand(2..5),
+    result_section_id: rand(2..19),
     user_id: 1
   )
 end
 
 5.times do
   Record.create!(
-    date: "2021-04-01",
+    date: "2021-05-22",
     hour_id: rand(2..5),
-    pair_id: rand(2..5),
+    pair_id: 51,
     ashi_id: rand(2..5),
     order_id: rand(2..5),
     line_main_id: rand(2..5),
     line_sub_id: rand(2..5),
     indi_main_id: rand(2..5),
     indi_sub_id: rand(2..5),
-    price_renge: 25,
+    price_renge: rand(-100..150),
     entry_basis: "text text",
     result_content: "text text",
-    result_section_id: rand(2..5),
+    result_section_id: rand(2..19),
     user_id: 1
   )
 end
 
 5.times do
   Record.create!(
-    date: "2021-04-01",
+    date: "2021-06-12",
     hour_id: rand(2..5),
-    pair_id: rand(2..5),
+    pair_id: 65,
     ashi_id: rand(2..5),
     order_id: rand(2..5),
     line_main_id: rand(2..5),
     line_sub_id: rand(2..5),
     indi_main_id: rand(2..5),
     indi_sub_id: rand(2..5),
-    price_renge: -15,
+    price_renge: rand(-100..150),
     entry_basis: "text text",
     result_content: "text text",
-    result_section_id: rand(2..5),
+    result_section_id: rand(2..19),
     user_id: 1
   )
 end
 
 5.times do
   Record.create!(
-    date: "2021-04-05",
+    date: "2021-06-19",
     hour_id: rand(2..5),
-    pair_id: rand(2..5),
+    pair_id: 91,
     ashi_id: rand(2..5),
     order_id: rand(2..5),
     line_main_id: rand(2..5),
     line_sub_id: rand(2..5),
     indi_main_id: rand(2..5),
     indi_sub_id: rand(2..5),
-    price_renge: 20,
+    price_renge: rand(-100..150),
     entry_basis: "text text",
     result_content: "text text",
-    result_section_id: rand(2..5),
+    result_section_id: rand(2..19),
     user_id: 1
   )
 end
 
 5.times do
   Record.create!(
-    date: "2021-04-05",
+    date: "2021-06-27",
     hour_id: rand(2..5),
-    pair_id: rand(2..5),
+    pair_id: 108,
     ashi_id: rand(2..5),
     order_id: rand(2..5),
     line_main_id: rand(2..5),
     line_sub_id: rand(2..5),
     indi_main_id: rand(2..5),
     indi_sub_id: rand(2..5),
-    price_renge: -22,
+    price_renge: rand(-100..150),
     entry_basis: "text text",
     result_content: "text text",
-    result_section_id: rand(2..5),
+    result_section_id: rand(2..19),
     user_id: 1
   )
 end
 
 5.times do
   Record.create!(
-    date: "2021-11-01",
+    date: "2021-08-12",
     hour_id: rand(2..5),
-    pair_id: rand(2..5),
+    pair_id: 51,
     ashi_id: rand(2..5),
     order_id: rand(2..5),
     line_main_id: rand(2..5),
     line_sub_id: rand(2..5),
     indi_main_id: rand(2..5),
     indi_sub_id: rand(2..5),
-    price_renge: 25,
+    price_renge: rand(-100..150),
     entry_basis: "text text",
     result_content: "text text",
-    result_section_id: rand(2..5),
+    result_section_id: rand(2..19),
     user_id: 1
   )
 end
 
 5.times do
   Record.create!(
-    date: "2021-11-01",
+    date: "2021-08-14",
     hour_id: rand(2..5),
-    pair_id: rand(2..5),
+    pair_id: 65,
     ashi_id: rand(2..5),
     order_id: rand(2..5),
     line_main_id: rand(2..5),
     line_sub_id: rand(2..5),
     indi_main_id: rand(2..5),
     indi_sub_id: rand(2..5),
-    price_renge: -15,
+    price_renge: rand(-100..150),
     entry_basis: "text text",
     result_content: "text text",
-    result_section_id: rand(2..5),
+    result_section_id: rand(2..19),
     user_id: 1
   )
 end
 
 5.times do
   Record.create!(
-    date: "2021-11-05",
+    date: "2021-08-22",
     hour_id: rand(2..5),
-    pair_id: rand(2..5),
+    pair_id: 91,
     ashi_id: rand(2..5),
     order_id: rand(2..5),
     line_main_id: rand(2..5),
     line_sub_id: rand(2..5),
     indi_main_id: rand(2..5),
     indi_sub_id: rand(2..5),
-    price_renge: 20,
+    price_renge: rand(-100..150),
     entry_basis: "text text",
     result_content: "text text",
-    result_section_id: rand(2..5),
+    result_section_id: rand(2..19),
     user_id: 1
   )
 end
 
 5.times do
   Record.create!(
-    date: "2021-11-05",
+    date: "2021-09-14",
     hour_id: rand(2..5),
-    pair_id: rand(2..5),
+    pair_id: 108,
     ashi_id: rand(2..5),
     order_id: rand(2..5),
     line_main_id: rand(2..5),
     line_sub_id: rand(2..5),
     indi_main_id: rand(2..5),
     indi_sub_id: rand(2..5),
-    price_renge: -22,
+    price_renge: rand(-100..150),
     entry_basis: "text text",
     result_content: "text text",
-    result_section_id: rand(2..5),
+    result_section_id: rand(2..19),
     user_id: 1
   )
 end
+
+5.times do
+  Record.create!(
+    date: "2021-08-19",
+    hour_id: rand(2..5),
+    pair_id: 51,
+    ashi_id: rand(2..5),
+    order_id: rand(2..5),
+    line_main_id: rand(2..5),
+    line_sub_id: rand(2..5),
+    indi_main_id: rand(2..5),
+    indi_sub_id: rand(2..5),
+    price_renge: rand(-100..150),
+    entry_basis: "text text",
+    result_content: "text text",
+    result_section_id: rand(2..19),
+    user_id: 1
+  )
+end
+
+5.times do
+  Record.create!(
+    date: "2021-08-22",
+    hour_id: rand(2..5),
+    pair_id: 65,
+    ashi_id: rand(2..5),
+    order_id: rand(2..5),
+    line_main_id: rand(2..5),
+    line_sub_id: rand(2..5),
+    indi_main_id: rand(2..5),
+    indi_sub_id: rand(2..5),
+    price_renge: rand(-100..150),
+    entry_basis: "text text",
+    result_content: "text text",
+    result_section_id: rand(2..19),
+    user_id: 1
+  )
+end
+
+5.times do
+  Record.create!(
+    date: "2021-08-24",
+    hour_id: rand(2..5),
+    pair_id: 91,
+    ashi_id: rand(2..5),
+    order_id: rand(2..5),
+    line_main_id: rand(2..5),
+    line_sub_id: rand(2..5),
+    indi_main_id: rand(2..5),
+    indi_sub_id: rand(2..5),
+    price_renge: rand(-100..150),
+    entry_basis: "text text",
+    result_content: "text text",
+    result_section_id: rand(2..19),
+    user_id: 1
+  )
+end
+
+5.times do
+  Record.create!(
+    date: "2021-09-04",
+    hour_id: rand(2..5),
+    pair_id: 108,
+    ashi_id: rand(2..5),
+    order_id: rand(2..5),
+    line_main_id: rand(2..5),
+    line_sub_id: rand(2..5),
+    indi_main_id: rand(2..5),
+    indi_sub_id: rand(2..5),
+    price_renge: rand(-100..150),
+    entry_basis: "text text",
+    result_content: "text text",
+    result_section_id: rand(2..19),
+    user_id: 1
+  )
+end
+
+5.times do
+  Record.create!(
+    date: "2021-10-04",
+    hour_id: rand(2..5),
+    pair_id: 51,
+    ashi_id: rand(2..5),
+    order_id: rand(2..5),
+    line_main_id: rand(2..5),
+    line_sub_id: rand(2..5),
+    indi_main_id: rand(2..5),
+    indi_sub_id: rand(2..5),
+    price_renge: rand(-100..150),
+    entry_basis: "text text",
+    result_content: "text text",
+    result_section_id: rand(2..19),
+    user_id: 1
+  )
+end
+
+5.times do
+  Record.create!(
+    date: "2021-10-12",
+    hour_id: rand(2..5),
+    pair_id: 65,
+    ashi_id: rand(2..5),
+    order_id: rand(2..5),
+    line_main_id: rand(2..5),
+    line_sub_id: rand(2..5),
+    indi_main_id: rand(2..5),
+    indi_sub_id: rand(2..5),
+    price_renge: rand(-100..150),
+    entry_basis: "text text",
+    result_content: "text text",
+    result_section_id: rand(2..19),
+    user_id: 1
+  )
+end
+
+5.times do
+  Record.create!(
+    date: "2021-10-15",
+    hour_id: rand(2..5),
+    pair_id: 91,
+    ashi_id: rand(2..5),
+    order_id: rand(2..5),
+    line_main_id: rand(2..5),
+    line_sub_id: rand(2..5),
+    indi_main_id: rand(2..5),
+    indi_sub_id: rand(2..5),
+    price_renge: rand(-100..150),
+    entry_basis: "text text",
+    result_content: "text text",
+    result_section_id: rand(2..19),
+    user_id: 1
+  )
+end
+
+5.times do
+  Record.create!(
+    date: "2021-10-18",
+    hour_id: rand(2..5),
+    pair_id: 108,
+    ashi_id: rand(2..5),
+    order_id: rand(2..5),
+    line_main_id: rand(2..5),
+    line_sub_id: rand(2..5),
+    indi_main_id: rand(2..5),
+    indi_sub_id: rand(2..5),
+    price_renge: rand(-100..150),
+    entry_basis: "text text",
+    result_content: "text text",
+    result_section_id: rand(2..19),
+    user_id: 1
+  )
+end
+
+5.times do
+  Record.create!(
+    date: "2021-10-20",
+    hour_id: rand(2..5),
+    pair_id: 51,
+    ashi_id: rand(2..5),
+    order_id: rand(2..5),
+    line_main_id: rand(2..5),
+    line_sub_id: rand(2..5),
+    indi_main_id: rand(2..5),
+    indi_sub_id: rand(2..5),
+    price_renge: rand(-100..150),
+    entry_basis: "text text",
+    result_content: "text text",
+    result_section_id: rand(2..19),
+    user_id: 1
+  )
+end
+
+5.times do
+  Record.create!(
+    date: "2021-11-14",
+    hour_id: rand(2..5),
+    pair_id: 65,
+    ashi_id: rand(2..5),
+    order_id: rand(2..5),
+    line_main_id: rand(2..5),
+    line_sub_id: rand(2..5),
+    indi_main_id: rand(2..5),
+    indi_sub_id: rand(2..5),
+    price_renge: rand(-100..150),
+    entry_basis: "text text",
+    result_content: "text text",
+    result_section_id: rand(2..19),
+    user_id: 1
+  )
+end
+
+5.times do
+  Record.create!(
+    date: "2021-11-24",
+    hour_id: rand(2..5),
+    pair_id: 91,
+    ashi_id: rand(2..5),
+    order_id: rand(2..5),
+    line_main_id: rand(2..5),
+    line_sub_id: rand(2..5),
+    indi_main_id: rand(2..5),
+    indi_sub_id: rand(2..5),
+    price_renge: rand(-100..150),
+    entry_basis: "text text",
+    result_content: "text text",
+    result_section_id: rand(2..19),
+    user_id: 1
+  )
+end
+
+5.times do
+  Record.create!(
+    date: "2021-12-01",
+    hour_id: rand(2..5),
+    pair_id: 108,
+    ashi_id: rand(2..5),
+    order_id: rand(2..5),
+    line_main_id: rand(2..5),
+    line_sub_id: rand(2..5),
+    indi_main_id: rand(2..5),
+    indi_sub_id: rand(2..5),
+    price_renge: rand(-100..150),
+    entry_basis: "text text",
+    result_content: "text text",
+    result_section_id: rand(2..19),
+    user_id: 1
+  )
+end
+
+5.times do
+  Record.create!(
+    date: "2021-12-02",
+    hour_id: rand(2..5),
+    pair_id: 51,
+    ashi_id: rand(2..5),
+    order_id: rand(2..5),
+    line_main_id: rand(2..5),
+    line_sub_id: rand(2..5),
+    indi_main_id: rand(2..5),
+    indi_sub_id: rand(2..5),
+    price_renge: rand(-100..150),
+    entry_basis: "text text",
+    result_content: "text text",
+    result_section_id: rand(2..19),
+    user_id: 1
+  )
+end
+
+5.times do
+  Record.create!(
+    date: "2021-12-04",
+    hour_id: rand(2..5),
+    pair_id: 65,
+    ashi_id: rand(2..5),
+    order_id: rand(2..5),
+    line_main_id: rand(2..5),
+    line_sub_id: rand(2..5),
+    indi_main_id: rand(2..5),
+    indi_sub_id: rand(2..5),
+    price_renge: rand(-100..150),
+    entry_basis: "text text",
+    result_content: "text text",
+    result_section_id: rand(2..19),
+    user_id: 1
+  )
+end
+
+5.times do
+  Record.create!(
+    date: "2021-11-24",
+    hour_id: rand(2..5),
+    pair_id: 91,
+    ashi_id: rand(2..5),
+    order_id: rand(2..5),
+    line_main_id: rand(2..5),
+    line_sub_id: rand(2..5),
+    indi_main_id: rand(2..5),
+    indi_sub_id: rand(2..5),
+    price_renge: rand(-100..150),
+    entry_basis: "text text",
+    result_content: "text text",
+    result_section_id: rand(2..19),
+    user_id: 2
+  )
+end
+
+5.times do
+  Record.create!(
+    date: "2021-12-01",
+    hour_id: rand(2..5),
+    pair_id: 108,
+    ashi_id: rand(2..5),
+    order_id: rand(2..5),
+    line_main_id: rand(2..5),
+    line_sub_id: rand(2..5),
+    indi_main_id: rand(2..5),
+    indi_sub_id: rand(2..5),
+    price_renge: rand(-100..150),
+    entry_basis: "text text",
+    result_content: "text text",
+    result_section_id: rand(2..19),
+    user_id: 2
+  )
+end
+
+5.times do
+  Record.create!(
+    date: "2021-12-02",
+    hour_id: rand(2..5),
+    pair_id: 51,
+    ashi_id: rand(2..5),
+    order_id: rand(2..5),
+    line_main_id: rand(2..5),
+    line_sub_id: rand(2..5),
+    indi_main_id: rand(2..5),
+    indi_sub_id: rand(2..5),
+    price_renge: rand(-100..150),
+    entry_basis: "text text",
+    result_content: "text text",
+    result_section_id: rand(2..19),
+    user_id: 2
+  )
+end
+
+5.times do
+  Record.create!(
+    date: "2021-12-04",
+    hour_id: rand(2..5),
+    pair_id: 65,
+    ashi_id: rand(2..5),
+    order_id: rand(2..5),
+    line_main_id: rand(2..5),
+    line_sub_id: rand(2..5),
+    indi_main_id: rand(2..5),
+    indi_sub_id: rand(2..5),
+    price_renge: rand(-100..150),
+    entry_basis: "text text",
+    result_content: "text text",
+    result_section_id: rand(2..19),
+    user_id: 2
+  )
+end
+

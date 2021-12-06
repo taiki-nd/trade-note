567928864e080cffe44ad387f16cb0d40f3ff046
Taiki Noda <taiki.nd.real@gmail.com>
2021-12-07 00:33:46 +0900

    枚数制限

--- app/models/record.rb
+++ app/models/record.rb
@@ -20,6 +20,10 @@ class Record < ApplicationRecord
     validates :result_section_id
   end
 
+  FILE_NUMBER_LIMIT = 3
+
+  validate :validate_number_of_files
+
   belongs_to :user
   has_many_attached :images
   has_many :comment_records, dependent: :destroy
@@ -35,5 +39,12 @@ class Record < ApplicationRecord
   belongs_to :indi_sub, optional: true
   belongs_to :order, optional: true
   belongs_to :result_section, optional: true
+
+  private
+
+  def validate_number_of_files
+    return if images.length <= FILE_NUMBER_LIMIT
+    errors.add(:images, "に添付できる画像は#{FILE_NUMBER_LIMIT}件までです。")
+  end
 
 end

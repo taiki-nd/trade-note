c9587a903dd026d6b3856d6e1081eb784ed3aecb
Taiki Noda <taiki.nd.real@gmail.com>
2021-12-12 20:56:34 +0900

    postカテゴリーの修正

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,11 +4,10 @@ class Category < ActiveHash::Base
     { id: 2, name: 'fx' },
     { id: 3, name: '仮想通貨' },
     { id: 4, name: '株' },
-    { id: 5, name: 'トレードノート' },
-    { id: 6, name: 'トレード手法' },
-    { id: 7, name: 'トレード日誌' },
-    { id: 8, name: '証券会社、取引所' },
-    { id: 9, name: 'その他' }
+    { id: 5, name: '取引手法' },
+    { id: 6, name: 'トレード日誌' },
+    { id: 7, name: '証券会社、取引所' },
+    { id: 8, name: 'その他' }
   ]
  
    include ActiveHash::Associations

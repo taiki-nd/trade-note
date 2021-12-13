f5274466f51b6953ddf26afbd531c7044f6541e3
Taiki Noda <taiki.nd.real@gmail.com>
2021-12-13 11:54:26 +0900

    カラム追加

---
+++ db/migrate/20211213024604_add_column_to_posts.rb
@@ -0,0 +1,9 @@
+class AddColumnToPosts < ActiveRecord::Migration[6.1]
+  def change
+    add_column :posts, :status_id, :integer, default: 0
+    add_column :posts, :keyword1, :string
+    add_column :posts, :keyword2, :string
+    add_column :posts, :keyword3, :string
+    add_column :posts, :description, :text
+  end
+end

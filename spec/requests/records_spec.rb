2f4c9e2d75de4a6fe9ad27d0c134e2bf375591b4
Taiki Noda <taiki.nd.real@gmail.com>
2021-12-17 23:06:10 +0900

    rspec show

--- spec/requests/records_spec.rb
+++ spec/requests/records_spec.rb
@@ -15,4 +15,42 @@ describe RecordsController, type: :request do
       expect(response.body).to include(@record.pair.name)
     end
   end
+
+  describe 'GET #show' do
+    it 'showアクションにリクエストすると正常にレスポンスが返ってくる' do 
+      get record_path(@record)
+      expect(response.status).to eq 200
+    end
+    it 'showアクションにリクエストするとレスポンスに投稿済みのrecordのpairが存在する' do 
+      get record_path(@record)
+      expect(response.body).to include(@record.pair.name)
+    end
+    it 'showアクションにリクエストするとレスポンスに投稿済みのrecodのdateが存在する' do 
+      get record_path
+      expect(response.body).to include(@record.date)
+    end
+    it 'showアクションにリクエストするとレスポンスに投稿済みのrecodのhourが存在する' do 
+      get record_path
+      expect(response.body).to include(@record.hour.name)
+    end
+    it 'showアクションにリクエストするとレスポンスに投稿済みのrecodのresult_sectionが存在する' do 
+      get record_path
+      expect(response.body).to include(@record.result_section.name)
+    end
+    it 'showアクションにリクエストするとレスポンスに投稿済みのrecodのresult_contentが存在する' do 
+      get record_path
+      expect(response.body).to include(@record.result_content)
+    end
+    it 'showアクションにリクエストするとレスポンスに投稿済みのrecodのprice_rengeが存在する' do 
+      get record_path
+      expect(response.body).to include(@record.price_renge)
+    end
+    it 'showアクションにリクエストするとレスポンスに投稿済みのrecodのashiが存在する' do 
+      get record_path
+      expect(response.body).to include(@record.ashi.name)
+    end
+    it 'showアクションにリクエストするとレスポンスにコメント一覧表示部分が存在する' do 
+    end
+  end 
+
 end

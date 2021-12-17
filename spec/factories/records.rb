24fee7b10c4dabd643421567dc9f6a948f4dc229
Taiki Noda <taiki.nd.real@gmail.com>
2021-12-17 18:20:43 +0900

    records factorybotの修正

--- spec/factories/records.rb
+++ spec/factories/records.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :record do
-    date              {Faker::Date.between(from: '2021-09-23', to: '20121-12-15')}
+    date              {Faker::Date.between(from: '2021-09-23', to: '2021-12-15')}
     pair_id           {Faker::Number.within(range: 2..50)}
     ashi_id           {Faker::Number.within(range: 2..10)}
     price_renge       {Faker::Number.between(from: -100, to: 100)}

229310353ec252b270f92347c54139503bb26772
Taiki Noda <taiki.nd.real@gmail.com>
2021-12-17 19:58:05 +0900

    recordの表示順の編集

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -7,7 +7,7 @@ class RecordsController < ApplicationController
   before_action :set_q, only: [:index, :search]
 
   def index
-    @records = Record.includes(:user).order(date: "DESC").order(hour_id: "DESC").page(params[:page]).per(16)
+    @records = Record.includes(:user).order(date: "DESC").order(hour_id: "DESC").order("created_at DESC").page(params[:page]).per(16)
   end
 
   def new
--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -1,6 +1,6 @@
 class StaticPageController < ApplicationController
   def top
-    @records = Record.includes(:user).order(date: "DESC").order(hour_id: "DESC").limit(4)
+    @records = Record.includes(:user).order(date: "DESC").order(hour_id: "DESC").order("created_at DESC").limit(4)
     @posts = Post.includes(:user).where(status_id: 2).order("created_at DESC").limit(4)
   end
 

4c234f3b8a759f7848d4f96e3b5272d6dde3993e
Taiki Noda <taiki.nd.real@gmail.com>
2021-12-23 19:57:44 +0900

    修正

--- app/javascript/components/Like.js
+++ app/javascript/components/Like.js
@@ -4,16 +4,7 @@ import styled from 'styled-components'
 
 class Like extends React.Component {
 
-  getInitialState () {
-    return {
-        counts: 0,         //いいねの合計数
-        is_liked: false    //いいねされているかどうかの状態
-    }
-  }
-
-  LikeAjax () {
-    console.log("clicked")
-  } 
+ 
 
   render () {
     return (
@@ -21,6 +12,10 @@ class Like extends React.Component {
       <LikeBtn onClick={this.LikeAjax}>
         NiceTrade!
       </LikeBtn>
+
+      <NonLikeBtn onClick={this.LikeAjax}>
+        NiceTrade!
+      </NonLikeBtn>
       </>
     );
   }
@@ -42,5 +37,24 @@ const LikeBtn = styled.div`
   &:active {
     background-image: -webkit-linear-gradient(20deg, #fd3838, #dbdbdb, #87ff41);
     background-image: linear-gradient(70deg, #fd3838, #dbdbdb, #87ff41);
+  }
+`
+
+const NonLikeBtn = styled.div`
+  width: fit-content;
+  text-decoration: none;
+  color: #000000;
+  padding: 0.5rem 1rem;
+  border: 1px solid #000000;
+  border-radius: 100vh;
+  background-image: -webkit-linear-gradient(20deg, #fd3838, #dbdbdb, #87ff41);
+  background-image: linear-gradient(70deg, #fd3838, #dbdbdb, #87ff41);
+  &:hover {
+    background-image: -webkit-linear-gradient(20deg, #ffffff, #ffffff);
+    background-image: linear-gradient(20deg, #ffffff, #ffffff);
+  }
+  &:active {
+    background-image: -webkit-linear-gradient(20deg, #ffffff, #ffffff);
+    background-image: linear-gradient(20deg, #ffffff, #ffffff);
   }
 `

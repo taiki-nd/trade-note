bcea6e80e2726a2c9ae11c0753c4af87166de153
Taiki Noda <taiki.nd.real@gmail.com>
2021-12-23 18:06:07 +0900

    スタイルの適用

--- app/javascript/components/Like.js
+++ app/javascript/components/Like.js
@@ -1,16 +1,26 @@
 import React from "react"
 import PropTypes from "prop-types"
+import styled from 'styled-components'
 
 class Like extends React.Component {
   render () {
     return (
       <>
-      <div>
-        NiceTrade
-      </div>
+      <LikeBtn>
+        NiceTrade!
+      </LikeBtn>
       </>
     );
   }
 }
 
 export default Like
+
+const LikeBtn = styled.div`
+  width: fit-content;
+  text-decoration: none;
+  color: #000000;
+  padding: 0.5rem 1rem;
+  border: 1px solid #000000;
+  border-radius: 100vh;
+`

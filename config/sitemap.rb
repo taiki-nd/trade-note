bce522a9a8d2e4c60d22d0a0a216b0715f63fd5f
Taiki Noda <taiki.nd.real@gmail.com>
2022-01-14 15:15:19 +0900

    サイトマップの修正

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,6 +1,7 @@
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "https://trade-note.jp"
 
+SitemapGenerator::Sitemap.create do
   add records_path, changefreq: 'always'
   Record.find_each do |record|
     add record_path(record), changefreq: 'daily', lastmod: record.updated_at
@@ -19,28 +20,4 @@ SitemapGenerator::Sitemap.default_host = "https://trade-note.jp"
   add privacy_policy_path, changefreq: 'monthly'
   add disclaimer_path, changefreq: 'monthly'
   add lp_path, changefreq: 'monthly'
-
-SitemapGenerator::Sitemap.create do
-  # Put links creation logic here.
-  #
-  # The root path '/' and sitemap index file are added automatically for you.
-  # Links are added to the Sitemap in the order they are specified.
-  #
-  # Usage: add(path, options={})
-  #        (default options are used if you don't specify)
-  #
-  # Defaults: :priority => 0.5, :changefreq => 'weekly',
-  #           :lastmod => Time.now, :host => default_host
-  #
-  # Examples:
-  #
-  # Add '/articles'
-  #
-  #   add articles_path, :priority => 0.7, :changefreq => 'daily'
-  #
-  # Add all articles:
-  #
-  #   Article.find_each do |article|
-  #     add article_path(article), :lastmod => article.updated_at
-  #   end
 end
